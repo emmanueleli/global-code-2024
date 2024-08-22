f02b7026aae28935d7024f9382dd13563e05eb0d
Emmanuel Eli <emmanueleli120@gmail.com>
2024-08-22 15:21:54 +0100

    the weather

---
+++ w1d4/app.py
@@ -0,0 +1,15 @@
+import pyowm
+
+owm = pyowm.OWM('7bb562e65e4633b6fb62417e3b57e7fb')
+mgr = owm.weather_manager()
+observation = mgr.weather_at_place('London,uk')
+#w = observation.get_weather()
+w = observation.weather
+
+#w.get_wind()
+#w.get_humidity()
+
+print(w.wind())
+print(w.humidity)
+#print(w.rain)
+#print(w.heat_index)
